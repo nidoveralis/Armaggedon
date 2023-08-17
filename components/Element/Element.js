c4f684b4078cd6ecbc9aef855f6036e4a2786a08
Nidoveralis <nidoveralis@gmail.com>
2023-08-17 12:13:48 +0400

    feat:  elements and styles

--- components/Element/Element.js
+++ components/Element/Element.js
@@ -2,28 +2,29 @@ import Astro from '../../images/astro.png';
 import NextImage from 'next/image';
 import styles from './Element.module.css';
 
-const Element = ({card}) => {
+const Element = ({el}) => {
+  //estimated_diameter.kilometers.estimated_diameter_max
 
-  const bigAstro = {width: '36px', heigth: '40px' }
-  const smallAstro = {width: '22px', heigth: '24px '}
+
+  const bigAstro = {width: '36', heigth: '40' }
+  const smallAstro = {width: '22', heigth: '24'}
 
   const dang='⚠ Опасен';
-  const astroSize = card.size>=100 ? bigAstro : smallAstro;
-
+  const astroSize = el.diameter>=100 ? bigAstro : smallAstro;
 
   return(
     <li className={styles.element}>
-      <p className={styles.element__date}>{card.data}</p>
+      <p className={styles.element__date}>{el.data}</p>
       <div className={styles.element__info}>
-        <div className={styles.element__distance}>{card.distance}</div>
+        <div className={styles.element__distance}>{el.estimated_diameter_max}</div>
         <NextImage src={Astro} className={styles.element__img} alt='Астероид' {...astroSize} />
         <div className={styles.element__description}>
-          <p className={styles.element__name}>{card.name}</p>
-          <p className={styles.element__size}>{`Ø ${card.size} м`}</p>
+          <p className={styles.element__name}>{el.name}</p>
+          <p className={styles.element__diameter}>{`Ø ${el.estimated_diameter} м`}</p>
         </div>
       </div>
-      <button className={styles.element__button}>ЗАКАЗАТЬ</button>
-      <p className={styles.element__danger}>{card.danger && dang}</p>
+      <button className={`${styles.element__button} ${el.order ? styles.element__button_active : ''}`}>{el.order ? 'В КОРЗИНЕ' : 'ЗАКАЗАТЬ'}</button>
+      <p className={styles.element__danger}>{el.is_potentially_hazardous_asteroid && dang}</p>
     </li>
   )
 }
--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,34 +1,64 @@
+import React, { useState, useEffect } from 'react';
 import styles from './Main.module.css';
 import Element from '../Element/Element';
 
-const Main = () => {
+
+
+const Main = ({data}) => {
+  console.log(data)
+  const [arrAstro, setArrAstro] = React.useState([]);
 
   const cards = [
     {
-      _id:0,
+      id:0,
       data: '12 сент 2023',
-      distance: '5 652 475 км',
+      estimated_diameter_max: '5 652 475 км',
       name: '2021 FQ',
-      size: '234',
-      danger: true
+      estimated_diameter: '234',
+      is_potentially_hazardous_asteroid
+: true,
+      order: false
     },
     {
-      _id:1,
+      id:1,
       data: '12 сент 2023',
-      distance: '12 652 475 км',
+      estimated_diameter_max: '12 652 475 км',
       name: '2021 FQ',
-      size: '85',
-      danger: false
+      estimated_diameter: '85',
+      is_potentially_hazardous_asteroid
+: false,
+      order: true
+    },
+    {
+      id:3,
+      data: '12 сент 2023',
+      estimated_diameter_max: '5 652 475 км',
+      name: '2021 FQ',
+      estimated_diameter_max: '234',
+      is_potentially_hazardous_asteroid
+: true,
+      order: false
     }
   ]
 
+  //useEffect(() => {
+    //if (contacts) {
+      // Here you will need to map data to your format and set it to arrAstro;
+     // console.log(contacts); // this is a place holder
+   // }
+  //}, [contacts]);
+//const a = data.2023-09-12
   return(
     <div className={styles.asteroids} >
       <h2 className={styles.asteroids__title}>Ближайшие подлёты астероидов</h2>
-      <p className={styles.asteroids__subtitle}>в километрах | <span className={styles.asteroids__subtitle_line}>в лунных орбитах</span></p>
+      <div className={styles.asteroids__measurement}>
+        <div className={styles.asteroids__measurein}>в километрах |</div> 
+        <div className={styles.asteroids__measuremein}>в лунных орбитах</div>
+      </div>
       <ul className={styles.asteroids__list}>
-      {cards.map(card=> (
-          <Element key = {card._id} card={card} />
+      {cards.map(el=> (
+        
+          <Element key = {el.id} el={el} />
         ))}
       </ul>
   </div>
