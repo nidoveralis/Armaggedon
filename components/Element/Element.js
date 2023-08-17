a9d9bcbd0e9a73ea2b9af219b72cdf88ee54577b
Nidoveralis <nidoveralis@gmail.com>
2023-08-17 15:53:17 +0400

    feat: added data in Element

--- components/Element/Element.js
+++ components/Element/Element.js
@@ -5,22 +5,32 @@ import styles from './Element.module.css';
 const Element = ({el}) => {
   //estimated_diameter.kilometers.estimated_diameter_max
 
+  const [metricActive,setMetricActive]
 
+  const startDate = '2023-09-12'
   const bigAstro = {width: '36', heigth: '40' }
   const smallAstro = {width: '22', heigth: '24'}
 
   const dang='⚠ Опасен';
-  const astroSize = el.diameter>=100 ? bigAstro : smallAstro;
-
+  
+  const nameElement = [...(el.name.match(/\(.*?\)/g) || [])].map(s => s.slice(1, -1));
+  const diametrElement = Math.floor(el.estimated_diameter.meters.estimated_diameter_max);
+  const sizeElement = diametrElement>=100 ? bigAstro : smallAstro;
+  const distanceElement = {
+    kl:el.close_approach_data[0].miss_distance.kilometers.split('.')[0],
+    lunar:el.close_approach_data[0].miss_distance.lunar
+  };
+ const distanceKl = distanceElement.kl.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+console.log(el.is_potentially_hazardous_asteroid)
   return(
     <li className={styles.element}>
-      <p className={styles.element__date}>{el.data}</p>
+      <p className={styles.element__date}>{'12 сент 2023'}</p>
       <div className={styles.element__info}>
-        <div className={styles.element__distance}>{el.estimated_diameter_max}</div>
-        <NextImage src={Astro} className={styles.element__img} alt='Астероид' {...astroSize} />
+        <div className={styles.element__distance}>{distanceKl}</div>
+        <NextImage src={Astro} className={styles.element__img} alt='Астероид' {...sizeElement} />
         <div className={styles.element__description}>
-          <p className={styles.element__name}>{el.name}</p>
-          <p className={styles.element__diameter}>{`Ø ${el.estimated_diameter} м`}</p>
+          <p className={styles.element__name}>{nameElement}</p>
+          <p className={styles.element__diameter}>{`Ø ${diametrElement} м`}</p>
         </div>
       </div>
       <button className={`${styles.element__button} ${el.order ? styles.element__button_active : ''}`}>{el.order ? 'В КОРЗИНЕ' : 'ЗАКАЗАТЬ'}</button>
--- components/Main/Main.js
+++ components/Main/Main.js
@@ -2,52 +2,10 @@ import React, { useState, useEffect } from 'react';
 import styles from './Main.module.css';
 import Element from '../Element/Element';
 
-
+const startDate = '2023-09-12'
 
 const Main = ({data}) => {
-  console.log(data)
-  const [arrAstro, setArrAstro] = React.useState([]);
 
-  const cards = [
-    {
-      id:0,
-      data: '12 сент 2023',
-      estimated_diameter_max: '5 652 475 км',
-      name: '2021 FQ',
-      estimated_diameter: '234',
-      is_potentially_hazardous_asteroid
-: true,
-      order: false
-    },
-    {
-      id:1,
-      data: '12 сент 2023',
-      estimated_diameter_max: '12 652 475 км',
-      name: '2021 FQ',
-      estimated_diameter: '85',
-      is_potentially_hazardous_asteroid
-: false,
-      order: true
-    },
-    {
-      id:3,
-      data: '12 сент 2023',
-      estimated_diameter_max: '5 652 475 км',
-      name: '2021 FQ',
-      estimated_diameter_max: '234',
-      is_potentially_hazardous_asteroid
-: true,
-      order: false
-    }
-  ]
-
-  //useEffect(() => {
-    //if (contacts) {
-      // Here you will need to map data to your format and set it to arrAstro;
-     // console.log(contacts); // this is a place holder
-   // }
-  //}, [contacts]);
-//const a = data.2023-09-12
   return(
     <div className={styles.asteroids} >
       <h2 className={styles.asteroids__title}>Ближайшие подлёты астероидов</h2>
@@ -56,7 +14,7 @@ const Main = ({data}) => {
         <div className={styles.asteroids__measuremein}>в лунных орбитах</div>
       </div>
       <ul className={styles.asteroids__list}>
-      {cards.map(el=> (
+      {data[startDate].map(el=> (
         
           <Element key = {el.id} el={el} />
         ))}
