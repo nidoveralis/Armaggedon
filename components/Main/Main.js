34bf14e8f37bd71f5d3dd62b28d25a243eddf4c3
Nidoveralis <nidoveralis@gmail.com>
2023-08-19 12:28:42 +0400

    style: added and style image in component Main

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import NextImage from 'next/image';
+import EathImg from '../../images/eath.png';
 import styles from './Main.module.css';
 import Element from '../Element/Element';
 
@@ -20,6 +22,7 @@ const Main = ({data}) => {
 
   return(
     <div className={styles.asteroids} >
+        <NextImage src={EathImg} className={styles.main__img} alt='Планета земля' />
       <h2 className={styles.asteroids__title}>Ближайшие подлёты астероидов</h2>
       <div className={styles.asteroids__measurement}>
         <button className={`${styles.asteroids__measurein} ${metricKilometers ? styles.asteroids__measurein_active : styles.asteroids__measurein_off }`} onClick={handleClickMeasurement} disabled={metricKilometers} >в километрах</button> 
