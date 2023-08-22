24d0ce5c093cf776750d3ac3c6d5c203239ad71e
Nidoveralis <nidoveralis@gmail.com>
2023-08-22 22:06:25 +0400

    feat: created Asteroid and styled him

--- pages/asteroids/[id].js
+++ pages/asteroids/[id].js
@@ -1,11 +1,12 @@
 import { useRouter } from "next/router";
+import Asteroid from '../../components/Asteroud/Asteroid';
 
 export const getServerSideProps = async (context) => {
 console.log(context)
-  const url='https://api.nasa.gov/neo/rest/v1/neo/3328631?api_key=DEMO_KEY'
+  const url='https://api.nasa.gov/neo/rest/v1/neo/3328631?api_key=gjX3ndY5NQbyNkMqwR58egqxw3QptF2LZjeVGnn9'
   const response = await fetch(url);
   const data = await response.json();
-
+  console.log('llll')
   if (!data) {
     return {
       notFound: true,
@@ -13,26 +14,18 @@ console.log(context)
   }
 
   return {
-    props: { data: data.near_earth_objects || [] },
+    props: { data: data },
   }
 };
 
 
-const Astro=()=> {
-  const {query} = useRouter();
-  
-  //Там указаны данные астероида и список всех его сближений. 
-  //По каждому сближению: 
-  //скорость относительно Земли, relative_velocity
-  // время максимального сближения с Землей,  close_approach_data":[{"close_approach_date":"2015-09-08","close_approach_date_full":"2015-Sep-08 20:28",
-  //расстояние до Земли, distanceKl
-  // п
-  //о орбите вокруг чего летит. orbiting_body
-  //https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=DEMO_KEY
-
-//console.log(query,data)
+const Astro=({data})=> {
+ 
+console.log(data)
   return(
-    <h1>dlkdjdjddj</h1>
+    <>
+      <Asteroid data={data} />
+    </>
   )
 }
 
--- utils/constant.js
+++ utils/constant.js
@@ -13,7 +13,7 @@ const startDate = getTodayDate();
 const dang='&#9888; Опасен';
 const api_key = 'gjX3ndY5NQbyNkMqwR58egqxw3QptF2LZjeVGnn9';
 
-const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&api_key=DEMO_KEY`;
+const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&api_key=${api_key}`;
 
 
 export {bigAstro,smallAstro,url, getTodayDate, dang};
